fa7d60757aac76e78ed6e9590f722611c6750552
eugengev <eugengev@gmail.com>
2018-10-05 21:33:41 +0300

    commit 1

---
+++ js/common.js
@@ -0,0 +1,109 @@
+'use strict';
+if(!window.console) window.console = {};
+if(!window.console.memory) window.console.memory = function() {};
+if(!window.console.debug) window.console.debug = function() {};
+if(!window.console.error) window.console.error = function() {};
+if(!window.console.info) window.console.info = function() {};
+if(!window.console.log) window.console.log = function() {};
+
+// sticky footer
+//-----------------------------------------------------------------------------
+if(!Modernizr.flexbox) {
+  (function() {
+    var
+      $pageWrapper = $('#page-wrapper'),
+      $pageBody = $('#page-body'),
+      noFlexboxStickyFooter = function() {
+        $pageBody.height('auto');
+        if($pageBody.height() + $('#header').outerHeight() + $('#footer').outerHeight() < $(window).height()) {
+          $pageBody.height($(window).height() - $('#header').outerHeight() - $('#footer').outerHeight());
+        } else {
+          $pageWrapper.height('auto');
+        }
+      };
+    $(window).on('load resize', noFlexboxStickyFooter);
+  })();
+}
+if(ieDetector.ieVersion == 10 || ieDetector.ieVersion == 11) {
+  (function(){
+    var
+      $pageWrapper = $('#page-wrapper'),
+      $pageBody = $('#page-body'),
+      ieFlexboxFix = function() {
+        if($pageBody.addClass('flex-none').height() + $('#header').outerHeight() + $('#footer').outerHeight() < $(window).height()) {
+          $pageWrapper.height($(window).height());
+          $pageBody.removeClass('flex-none');
+        } else {
+          $pageWrapper.height('auto');
+        }
+      };
+    ieFlexboxFix();
+    $(window).on('load resize', ieFlexboxFix);
+  })();
+}
+
+$(function() {
+// placeholder
+//-----------------------------------------------------------------------------
+    $('input[placeholder], textarea[placeholder]').placeholder();
+
+      $('.js-main-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+        
+
+});
+
+var map;
+function initMap() {
+    var myLatLng = {lat: 49.4225715, lng: 26.9763337};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        scrollwheel: false,
+        center:  {lat: 49.4225715, lng: 26.9763337},  // Brooklyn.
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+        }
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'ТОчка на карте'
+    });
+}
+
